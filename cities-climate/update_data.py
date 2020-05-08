3a00fb1ad35c967218fb395692318267fc50642f
Luis Parravicini <lparravi@gmail.com>
2020-05-08 14:45:18 -0300

    moved script to cities-climate/

--- cities-climate/update_data.py
+++ cities-climate/update_data.py
@@ -1,52 +1,10 @@
 import requests
-from pathlib import Path
 import pandas as pd
 import hashlib
-import json
 import re
 from progress.bar import IncrementalBar
 import mwparserfromhell
-
-
-class Cache:
-    def __init__(self, data_path):
-        self.path = Path(data_path).expanduser()
-        self.path.mkdir(parents=True, exist_ok=True)
-
-    def get(self, session, url, params={}):
-        cache_object_path = self._path_for(url, params)
-        if cache_object_path.exists():
-            return self._read(cache_object_path)
-
-        res = session.get(url=url, params=params)
-        res.raise_for_status()
-        data = res.text
-
-        self._save(cache_object_path, data)
-
-        return data
-
-    def get_json(self, session, url, params={}):
-        data = self.get(session, url, params)
-        return json.loads(data)
-
-    def _read(self, path):
-        with open(path) as file:
-            return file.read()
-
-    def _save(self, path, data):
-        tmp_path = path.with_suffix('.tmp')
-        with open(tmp_path, 'w') as file:
-            file.write(data)
-        tmp_path.rename(path)
-
-    def _path_for(self, url, params):
-        query_data = (url + str(params)).encode('utf-8')
-        digest = hashlib.sha256(query_data).hexdigest()
-        path = Path(self.path, digest[0:2], digest)
-        path.parent.mkdir(parents=True, exist_ok=True)
-
-        return path
+from .cache import Cache
 
 
 def to_meters(feet):
